001dafe2f3818a471c6887aed5542bf1bd431f97
ArmandoTSW <jesus.cen@tecdesoftware.edu.mx>
2026-08-08 20:00:00 -0600

    Initialize Spring Boot project with PostgreSQL and Swagger

---
+++ src/test/java/com/project/gestiontareasapi/GestionTareasApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.gestiontareasapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GestionTareasApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
